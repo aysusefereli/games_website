17ca94da6f1dfe4f4e65896bdafe4a9ad0d01af3
unknown <sefereliaysu2@gmail.com>
2024-01-21 16:14:11 +0400

    games

--- src/store/slices/gameSlice.js
+++ src/store/slices/gameSlice.js
@@ -1,32 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+
+const storedState = JSON.parse(localStorage.getItem('reduxState')) || { favorites: [] };
 
 const initialState = {
-  favorites: [],
+  ...storedState,
 };
 
 const gameSlice = createSlice({
-    name: 'game',
-    initialState,
-    reducers: {
-        addToFavorites: (state, action) => {
-            const newGame = action.payload;
-            if (newGame && newGame.id) {
-                const existingGame = state.favorites.find((game) => game.id === newGame.id);
+  name: 'game',
+  initialState,
+  reducers: {
+    addToFavorites: (state, action) => {
+      const newGame = action.payload;
+      if (newGame && newGame.id) {
+        const existingGame = state.favorites.find((game) => game.id === newGame.id);
 
-                if (!existingGame) {
-                    state.favorites.push(newGame);
-                }
-            }
-        },
-        removeFromFavorites: (state, action) => {
-            state.favorites = state.favorites.filter((game) => game.id !== action.payload.id);
-        },
-        clearFavorites: (state) => {
-            state.favorites = [];
-        },
+        if (!existingGame) {
+          state.favorites.push(newGame);
+          updateLocalStorage(state.favorites);
+        }
+      }
     },
-})
+    removeFromFavorites: (state, action) => {
+      state.favorites = state.favorites.filter((game) => game.id !== action.payload.id);
+      updateLocalStorage(state.favorites);
+    },
+    clearFavorites: (state) => {
+      state.favorites = [];
+      updateLocalStorage(state.favorites);
+    },
+  },
+});
 
-export const { addToFavorites , removeFromFavorites , clearFavorites } = gameSlice.actions
+const updateLocalStorage = (favorites) => {
+  localStorage.setItem('reduxState', JSON.stringify({ favorites }));
+};
 
-export default gameSlice.reducer
+export const { addToFavorites, removeFromFavorites, clearFavorites } = gameSlice.actions;
+
+export default gameSlice.reducer;
